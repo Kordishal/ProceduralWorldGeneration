9250c17b979de834b0dc525213ffed1966c19e1c
Kordishal <jonaswaeber@gmail.com>
2016-06-23 13:37:51 +0200

    Added deity domains and planes.

---
+++ ProceduralWorldGeneration/ProceduralWorldGeneration/Input/MythObjectData.cs
@@ -0,0 +1,28 @@
+﻿using ProceduralWorldGeneration.MythObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProceduralWorldGeneration.Input
+{
+    class MythObjectData
+    {
+
+        public List<PrimordialForce> PrimordialForces = new List<PrimordialForce>();
+
+        public List<string> Domains = new List<string>();
+
+        public List<string> PlaneTypes = new List<string>();
+
+        public List<string> PlaneElements = new List<string>();
+
+
+        public MythObjectData()
+        {
+
+        }
+
+    }
+}
