9cc045de5d21aff3b46304a37f35caf223be6b21
Kordishal <jonaswaeber@gmail.com>
2016-06-30 23:29:06 +0200

    Added basic finite state machine implementation. Added root node to tree.

--- ProceduralWorldGeneration/ProceduralWorldGeneration/Input/ParserDefinition/Tree.cs
+++ ProceduralWorldGeneration/ProceduralWorldGeneration/Input/ParserDefinition/Tree.cs
@@ -10,9 +10,12 @@ namespace ProceduralWorldGeneration.Input.ParserDefinition
     {
 
 
+        public TreeNode<T> TreeRoot { get; set; }
+
+
         public Tree(T root_value) : base(root_value, null)
         {
-
+            TreeRoot = new TreeNode<T>(root_value, null);
         }
 
     }
---
+++ ProceduralWorldGeneration/ProceduralWorldGeneration/StateMachine/FiniteStateMachine.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProceduralWorldGeneration.StateMachine
+{
+    class FiniteStateMachine<S>
+    {
+        public delegate void StateTransitionCall();
+
+        protected S currentState { get; set; }
+        protected S previousState { get; set; }
+
+
+        protected Dictionary<StateTransition<S>, Delegate> TransitionTable { get; set; }
+
+
+        public FiniteStateMachine()
+        {
+            TransitionTable = new Dictionary<StateTransition<S>, Delegate>();
+        }
+
+        public void Initialise(S state)
+        {
+            currentState = state;
+        }
+
+        virtual public void Advance(S next_state)
+        {
+            StateTransition<S> temp_state_transition = new StateTransition<S>(currentState, next_state);
+
+            System.Delegate temp_delegate;
+            if (TransitionTable.TryGetValue(temp_state_transition, out temp_delegate))
+            {
+                if (temp_delegate != null)
+                {
+                    StateTransitionCall call = temp_delegate as StateTransitionCall;
+                    call();
+                }
+
+                previousState = currentState;
+                currentState = next_state;
+            }
+            else
+            {
+                throw new FiniteStateMachineException();
+            }
+        }
+
+        public void AddTransition(S initial_state, S end_state, StateTransitionCall call)
+        {
+            StateTransition<S> temp_transition = new StateTransition<S>(initial_state, end_state);
+
+            if (TransitionTable.ContainsKey(temp_transition))
+            {
+                return;
+            }
+
+            TransitionTable.Add(temp_transition, call);
+        }
+    }
+}
