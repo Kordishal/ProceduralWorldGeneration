537e2d1eee505dec1600a9a4e0066ff4df59fffa
Kordishal <jonaswaeber@gmail.com>
2016-07-04 18:14:38 +0200

    Added World, Nations, Civilisations, Species, Individuals.

---
+++ ProceduralWorldGeneration/ProceduralWorldGeneration/MythObjects/World.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProceduralWorldGeneration.DataStructure;
+
+namespace ProceduralWorldGeneration.MythObjects
+{
+    class World : ActionTakerMythObject
+    {
+        public override void addPossibleActions()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void takeAction(CreationMythState creation_myth, int current_year)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
